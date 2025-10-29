270e63118920d70df50ad7c34975e88f65864e43
steffen.waldmann <steffen.waldmann@unic.com>
2025-10-29 11:01:43 +0100

    fix: include content type header in POST requests

--- src-tauri/src/http_handler.rs
+++ src-tauri/src/http_handler.rs
@@ -4,7 +4,7 @@ pub fn send_http_request(request: Request<String>) -> Result<Response<String>, S
     match request.method().as_str() {
         "GET" => return get(request),
         "POST" => return post(request),
-        _ => return Err("Unsupported http method".to_owned())
+        _ => return Err("Unsupported http method".to_owned()),
     }
 }
 
@@ -17,28 +17,23 @@ fn get(request: Request<String>) -> Result<Response<String>, String> {
     let status = response.status().as_u16();
     let body = response.text().unwrap_or_default();
 
-    Ok(Response::builder()
-    .status(status)
-    .body(body)
-    .unwrap())
+    Ok(Response::builder().status(status).body(body).unwrap())
 }
 
 fn post(request: Request<String>) -> Result<Response<String>, String> {
     let client = reqwest::blocking::Client::new();
     let response = match client
         .post(request.uri().to_string())
+        .header("Content-Type", "application/json")
         .body(request.body().to_string())
-        .send() {
-            Ok(r) => r,
-            Err(e) => return Err(e.to_string()),
-        };
-
+        .send()
+    {
+        Ok(r) => r,
+        Err(e) => return Err(e.to_string()),
+    };
 
     let status = response.status().as_u16();
     let body = response.text().unwrap_or_default();
 
-    Ok(Response::builder()
-    .status(status)
-    .body(body)
-    .unwrap())
+    Ok(Response::builder().status(status).body(body).unwrap())
 }
