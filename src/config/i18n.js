e463f91619e61725227ee4411e06ddf6d5f67a8a
steffen.waldmann <steffen.waldmann@unic.com>
2026-01-01 15:43:02 +0100

    refactor: rename translations file

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslations from './locales/en/translation.json';
+import enTranslations from './locales/en.json';
 
 i18n.use(initReactI18next).init({
   resources: {
