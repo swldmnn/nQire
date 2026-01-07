a8a2a08a1c0ec2305969d9a643b98fe554882ecc
steffen.waldmann <steffen.waldmann@unic.com>
2026-01-07 13:04:59 +0100

    refactor: styling

--- src/views/ExportView.tsx
+++ src/views/ExportView.tsx
@@ -4,9 +4,9 @@ import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 
 function ExportView() {
     return (
-        <Box sx={{ padding: styles.spaces.large }}>
-            <EngineeringOutlinedIcon sx={{ fontSize: 28 }} />
-            <Typography variant='button'> Under construction</Typography>
+        <Box sx={{ padding: styles.spaces.large, display: 'flex', alignItems: 'center' }}>
+            <EngineeringOutlinedIcon sx={{ fontSize: 28, marginRight: styles.spaces.medium }} />
+            <Typography variant='button'>! Under construction !</Typography>
         </Box>
     )
 }
