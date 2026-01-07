d4b497c49d1d01dcf1ab393c9687ef7528b05aca
steffen.waldmann <steffen.waldmann@unic.com>
2026-01-07 12:07:55 +0100

    refactor: styling

--- src/views/ImportView.tsx
+++ src/views/ImportView.tsx
@@ -5,7 +5,8 @@ import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 function ImportView() {
     return (
         <Box sx={{ padding: styles.spaces.large }}>
-            <Typography variant='button' sx={{ fontSize: 18 }}><EngineeringOutlinedIcon sx={{ fontSize: 32 }} /> Under construction</Typography>
+            <EngineeringOutlinedIcon sx={{ fontSize: 28 }} />
+            <Typography variant='button'> Under construction</Typography>
         </Box>
     )
 }
