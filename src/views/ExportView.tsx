664f106791d1d7bc8bf7aaa0441aa7d44edca8c1
steffen.waldmann <steffen.waldmann@unic.com>
2026-01-07 12:03:15 +0100

    refactor: styling

--- src/views/ExportView.tsx
+++ src/views/ExportView.tsx
@@ -1,10 +1,11 @@
 import { Box, Typography } from "@mui/material"
 import { styles } from "../constants"
+import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 
 function ExportView() {
     return (
-        <Box sx={{ padding: styles.spaces.medium }}>
-            <Typography>Export not implemented yet</Typography>
+        <Box sx={{ padding: styles.spaces.large }}>
+            <Typography variant='button' sx={{ fontSize: 18 }}><EngineeringOutlinedIcon sx={{ fontSize: 32 }} /> Under construction</Typography>
         </Box>
     )
 }
--- src/views/ImportView.tsx
+++ src/views/ImportView.tsx
@@ -1,10 +1,11 @@
 import { Box, Typography } from "@mui/material"
 import { styles } from "../constants"
+import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 
 function ImportView() {
     return (
-        <Box sx={{ padding: styles.spaces.medium }}>
-            <Typography>Import not implemented yet </Typography>
+        <Box sx={{ padding: styles.spaces.large }}>
+            <Typography variant='button' sx={{ fontSize: 18 }}><EngineeringOutlinedIcon sx={{ fontSize: 32 }} /> Under construction</Typography>
         </Box>
     )
 }
